dac94412c2735695b7bbe5f2c00d39a0b1bd5489
DESKTOP-FL6VBV6\chlrl <chlrltjd159@naver.com>
2022-07-11 23:48:57 +0900

    chapter 6 : 어댑터 패턴
    - 기존 코드를 고치지 않고 외부 코드와 연결 시킬 수 있다
    - 중간에 어댑터를 두는 것이다

---
+++ gof/src/main/kotlin/com/pattern/gof/chapter2/AdapterPattern.kt
@@ -0,0 +1,51 @@
+package com.pattern.gof.chapter2
+
+//-----------------서드파티----------------------------
+interface UserService {
+
+    fun getUser(name: String): User
+}
+
+interface User {
+    fun checkPassword(password: String): Boolean
+}
+
+
+//-------------------로컬 계정 사용 중-----------------------------
+
+class LocalUser(val name: String, val password: String) {
+    fun checkPassword(password: String): Boolean {
+        return password == this.password
+    }
+}
+
+class LocalUserService {
+    private val repository = mapOf<String, LocalUser>("name" to LocalUser("name", "pasowrd"))
+
+    fun getUser(name: String): LocalUser {
+        return repository.getOrDefault(name, LocalUser("Anonymous","no-password"))
+    }
+}
+
+//-----------------------어댑터 적용-------------------------
+
+class AdapterUser(
+    private val localUser: LocalUser
+) : User {
+
+    override fun checkPassword(password: String): Boolean {
+        return localUser.checkPassword(password)
+    }
+
+}
+
+class AdapterUserService(
+    private val localUserService: LocalUserService
+) : UserService {
+
+    override fun getUser(name: String): User {
+        val user = localUserService.getUser(name)
+        return AdapterUser(user)
+    }
+
+}
